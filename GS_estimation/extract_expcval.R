264525d09e9ffa56f2df545c8e0f85baece75cd1
Alba Marino <96115604+albmarino@users.noreply.github.com>
2024-01-12 17:39:50 +0100

    Add files via upload
    update extract cval script

--- GS_estimation/extract_expcval.R
+++ GS_estimation/extract_expcval.R
@@ -1,6 +1,6 @@
 #!usr/bin/env Rscript
 
-# build the WLS model from the training dataset (Table S3) and extract expected C-values for the full dataset (Table S1) - tab-separated files
+# build the WLS model from the training dataset (Table S3), extract expected C-values for the full dataset (Table S1) and test reads effect - tab-separated files
 # example run: Rscript extract_expcval.R train_dataset.tsv full_dataset.tsv
 
 args <- commandArgs(trailingOnly = TRUE)
@@ -8,7 +8,7 @@ library(dplyr)
 
 trainset <- read.table(args[1], header=T,sep="\t")
 
-fullset <- read.table(args[2], header=T,sep="\t") %>% select(Species, Quast_ContigN50, Assembly_size)
+fullset <- read.table(args[2], header=T,sep="\t") %>% select(Species, Quast_ContigN50, Assembly_size, Reads_assembly)
 
 # WLS model with residuals of a linear model as weights 
 m1 <- lm(Cvalue~Assembly_size, data=trainset)
@@ -23,3 +23,23 @@ corrected_as <- data.frame(Species=n50set$Species, Expected_Cvalue = predict(wls
 
 fullset <- left_join(fullset, corrected_as, by="Species")
 write.table(fullset, file="assemblysize_expectedcvalues.tsv", ,quote=F, sep="\t", row.names=F, col.names=T)
+
+# test read type effect
+
+newheader <- "Reads_category"
+
+newdf <- data.frame(matrix(nrow=0, ncol=length(newheader)))
+for (i in trainset$Reads_assembly) {
+	if (i=="LR" || i == "LR-SR") {newdf <- rbind(newdf, "LR")}
+	else if (i=="SR") {newdf <- rbind(newdf, "SR")}
+	else {newdf <- rbind(newdf, "")}
+	}
+colnames(newdf) <- newheader
+
+trainset <- cbind(trainset, newdf)
+modreads <- lm(Cvalue ~ Assembly_size + Reads_category + Assembly_size:Reads_category, data=trainset)
+wt_modreads <- 1 / lm(abs(modreads$residuals) ~ modreads$fitted.values)$fitted.values^2
+wls_modreads <- lm(Cvalue ~ Assembly_size + Reads_category + Assembly_size:Reads_category, data = trainset, weights=wt_modreads)
+
+summary(wls_modreads)
+
