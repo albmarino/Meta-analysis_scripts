707d933b4b903c54d731e8075d73d98aa468b464
Alba Marino <96115604+albmarino@users.noreply.github.com>
2024-01-16 19:25:58 +0100

    Update make_coevol_table.R

--- Coevol/make_coevol_table.R
+++ Coevol/make_coevol_table.R
@@ -1,12 +1,15 @@
 #!/usr/bin/Rscript
 
-#Rscript make_coevol_table.R Actinopteri
+# Script to generate an input matrix for Coevol
+
+# Required input: clade name as 1st argument, list of species to remove output from gc3plots.R as 2nd argument, Table S2 as 3rd argument
+# Example usage: Rscript make_coevol_table.R Actinopteri sp2rm_50percgenespersp TableS2.tsv
 
 library(dplyr)
 args = commandArgs(trailingOnly=TRUE)
 
-df <- read.table(file="table_coevol.tsv", header=T,sep="\t")
-sp2rm <- read.table("sp2rm_50percgenespersp", header=F)$V1
+sp2rm <- read.table(args[2], header=F)$V1
+df <- read.table(file=args[3], header=T,sep="\t")
 df <- filter(df, ! Species %in% sp2rm)
 clade <- filter(df, Clade==args[1]) %>% select(Species, Unified_Cvalues_methods, BodyLength_cm, BasalMetRate_mLO2hr_pantheria, AgeatFirstBirth_d_pantheria, PopulationDensity_nperkm2_pantheria, MaxLongevity_y, Mass_g, Metabolic_rate_w, SexMaturity_d, DepthRange, Overall_repeat_bp, dnaPT_DNA_bp, dnaPT_RC_bp, dnaPT_LTR_bp, dnaPT_PLE_bp, dnaPT_LINE_bp, dnaPT_SINE_bp, dnaPT_Retroposon_bp, dnaPT_Unknown_bp, DNA_recent_bp, LINE_recent_bp, LTR_recent_bp, Overall_TEs_recent_bp, PLE_recent_bp, RC_recent_bp, Retroposon_recent_bp, SINE_recent_bp, Unknown_recent_bp)
 
